491a7371644870fee259af805c35d1f3104667c2
Giulio Cervera <giulio.cervera@gmail.com>
2012-08-21 12:30:15 +0200

    hikari: set width and height for cm10 bootanimation
    Change-Id: Ia7dd5f5b724a60e44485f4e79702e86f07a6993e

--- cm.mk
+++ cm.mk
@@ -6,12 +6,14 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
+# Boot Animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LT26w_1266-3189 BUILD_FINGERPRINT=SEMC/LT26w_1266-3189/LT26w:4.0.4/6.1.A.1.58/xft_zw:user/release-keys PRIVATE_BUILD_DESC="LT26w-user 4.0.4 6.1.A.1.58 xft_zw test-keys"
-
-TARGET_BOOTANIMATION_NAME := vertical-720x1280
 
 PRODUCT_NAME := cm_hikari
 PRODUCT_DEVICE := hikari
