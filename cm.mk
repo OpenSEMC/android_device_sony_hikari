d5feebeaeb4acf9af4af4b71a872fe5d105cb33e
Giulio Cervera <giulio.cervera@gmail.com>
2012-10-21 14:39:32 +0200

    hikari: update fingerprint
    Change-Id: Ie618304906ad011f4175c5219e07cec9868adba0

--- cm.mk
+++ cm.mk
@@ -13,7 +13,7 @@ TARGET_SCREEN_WIDTH := 720
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LT26w_1266-3189 BUILD_FINGERPRINT=SEMC/LT26w_1266-3189/LT26w:4.0.4/6.1.A.1.58/xft_zw:user/release-keys PRIVATE_BUILD_DESC="LT26w-user 4.0.4 6.1.A.1.58 xft_zw test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LT26w_1266-3204 BUILD_FINGERPRINT=SEMC/LT26w_1266-3204/LT26w:4.0.4/6.1.A.2.50/zfd_zw:user/release-keys PRIVATE_BUILD_DESC="LT26w-user 4.0.4 6.1.A.2.50 zfd_zw test-keys"
 
 PRODUCT_NAME := cm_hikari
 PRODUCT_DEVICE := hikari
