d1b88ccd9a1ece16ead81b04b12133257dc5fe3a
RaymanFX <raymanfx@gmail.com>
2013-06-28 14:58:51 +0200

    initial CFX device tree changes

--- cfx.mk
+++ cfx.mk
@@ -16,20 +16,20 @@
 
 $(call inherit-product, device/sony/hikari/full_hikari.mk)
 
-# Inherit CM common GSM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
+# Inherit CFX common GSM stuff.
+$(call inherit-product, vendor/cfx/config/gsm.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+# Enhanced NFC - not yet present in CFX
+#$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Boot Animation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# Inherit CM common Phone stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit CFX common Phone stuff.
+$(call inherit-product, vendor/cfx/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LT26w_1266-3204 BUILD_FINGERPRINT=SEMC/LT26w_1266-3204/LT26w:4.0.4/6.1.A.2.50/zfd_zw:user/release-keys PRIVATE_BUILD_DESC="LT26w-user 4.0.4 6.1.A.2.50 zfd_zw test-keys"
 
-PRODUCT_NAME := cm_hikari
+PRODUCT_NAME := cfx_hikari
 PRODUCT_DEVICE := hikari
